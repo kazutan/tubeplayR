5d001552c70d0a721edb147d6d00d9be7d34a5e3
kazutan <kazu@kz-md.net>
2017-05-27 23:06:48 +0900

    adapt youtube list and responsive window.

--- R/tubeplay.R
+++ R/tubeplay.R
@@ -1,24 +1,58 @@
 #' Play YouTube moview at RStudioServer
 #'
 #' @param url url that you want show Toutube movie
-#' @param width width of movie
-#' @param height height of movie
-#' @param background background color
+#' @param width ignored.
+#' @param height ignored.
+#' @param background ignored.
 #' @param viewer set viewer. See ?htmltools::html_print .
 #'
 #' @examples
 #' tubeplay() # play default set movie
 #' tubeplay("https://www.youtube.com/watch?v=0E00Zuayv9Q") # play "PPAP"
 #' @export
-tubeplay <- function(url = "https://www.youtube.com/watch?v=iOFZKwv_LfA", width = "560", height = "315",
+tubeplay <- function(url = "https://www.youtube.com/watch?v=iOFZKwv_LfA", width = "100%", height = "100%",
                      background = "white", viewer = getOption("viewer", utils::browseURL)) {
-  # make video_id
-  video_id <- gsub("^.*\\?v=", "", url)
+
+  # judge single or list
+  if(grepl("playlist?", url)) {
+    # set target for list
+    target <- gsub("^.*\\.com/", "", url)
+    target <- gsub("playlist", "videoseries", target)
+  }else{
+    # seto target for single
+    target <- gsub("^.*\\?v=", "", url)
+    target <- gsub("\\&.*$", "", target)
+  }
+
   # make_ui
   ui <- miniUI::miniPage(
-    htmltools::tags$iframe(width = width, height = height,
-                src = paste("https://www.youtube.com/embed", video_id, sep = "/"),
-                frameborder="0")
+    htmltools::tags$style(type = "text/css",
+                          paste(sep = "\n",
+                                "<!--",
+                                "div.iframeWrap {",
+                                htmltools::css(
+                                  position = "relative",
+                                  width = "100%",
+                                  `padding-top` = "56.25%"
+                                ),
+                                "}",
+                                "div.iframeWrap iframe {",
+                                htmltools::css(
+                                  position = "absolute",
+                                  top = 0,
+                                  left = 0,
+                                  width = "100%",
+                                  height = "100%"
+                                ),
+                                "}",
+                                "-->"
+                          )),
+    htmltools::tags$div(class = "iframeWrap",
+                        htmltools::tags$iframe(width = width,
+                                               height = height,
+                                               src = paste("https://www.youtube.com/embed", target, sep = "/"),
+                                               frameborder="0")
+    )
   )
   htmltools::html_print(ui)
 }
